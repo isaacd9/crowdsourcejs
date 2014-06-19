d3938fcaec71108bb90bf278432ccd31185cd21e
Ubuntu <ubuntu@ip-10-252-102-189.us-west-2.compute.internal>
2014-06-19 23:52:41 +0000

    intial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200);
+	res.setHeader("Content-Type","text/html");
+	res.end('<h1>It works</h1>');
+});
+server.listen(8000);
+
