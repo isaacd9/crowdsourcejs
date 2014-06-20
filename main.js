1f2fce65f6800722e697707ee3b9afa1ce9930ff
isaacd9 <isaacd9@gmail.com>
2014-06-20 08:06:57 +0000

    [Master] Wrote a working routing infrastructure. Need to refactor so some stuff is out of the way in controllers and public is seperate.

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
 var views = require('./views');
 
 var server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html' });
 	
-	views.renderView(res, "index", {what: "It"});
-});
+	var uri = url.parse(req.url).pathname;
+	if(uri == "/" || uri == "/view" || uri == "" ) {
+		res.writeHead(200, {'Content-Type': 'text/html' });
+		views.renderView(res, "viewquestion");
+		return;
+	}
+	if(uri.substr(0,7) == "/public") {
+		filename=path.join(process.cwd(), uri);
+		fs.exists(filename, function(exists) {
+			if(!exists) {
+				res.writeHead(404, {"Content-Type": 'text/plain'});
+				res.write("Error 404!");
+				res.end();
+				return;
+				}
+		fs.readFile(filename, "binary", function(err,file) {
+			if (err) throw err
+			res.writeHead(200);
+			res.write(file, "binary");
+			res.end();
+			});
+		});
+	}
+
+		else
+			{	
+				res.writeHead(404, {"Content-Type": 'text/plain'});
+				res.write("Error 404!");
+				res.end();
+			}
+	});
 server.listen(8000);
 
