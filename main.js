7c875f44765f774c52b48f1e39a2af950c199dfd
isaacd9 <isaacd9@gmail.com>
2014-06-20 00:00:45 +0000

    [Master] Just changed some punctuation

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 	res.writeHead(200);
-	res.setHeader("Content-Type","text/html");
+	res.setHeader("Content-Type",'text/html');
 	res.end('<h1>It works</h1>');
 });
 server.listen(8000);
